46cfd7361db5bf8153e753228b91897c7bcd39f5
小崎優吏 <yuri@ozakiyuurinoMacBook-Pro.local>
2017-10-14 15:48:04 +0900

    グループの編集機能と削除機能の追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
 
   def index
     @group = Group.find(params[:group_id])
+    @users = User.order("created_at ASC")
     @groups = Group.order("created_at ASC")
     @message = Message.new
     @group__search = Group.new
@@ -12,12 +13,24 @@ class MessagesController < ApplicationController
     @group = Group.find(params[:group_id])
 
     if @message.save
+      redirect_to group_messages_path(@group)
+    end
+  end
+
+  def edit
+    @message = Message.find(params[:id])
+  end
+
+  def update
+    @message = Message.find(params[:id])
+
+    if @message.update
       redirect_to group_messages_path
     end
   end
 
   private
   def message_params
-    params.require(:message).permit(:text, :image).merge(group_id: params[:group_id])
+    params.require(:message).permit(:text, :image).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.order("created_at ASC")
+    @groups = Group.order("created_at ASC")
+    @user = User.find(params[:id])
+    @message = Message.new
+    @group__search = Group.new
+  end
 end
